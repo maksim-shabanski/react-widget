4b857c7b168e120a77b592f3318b231b7919aadb
Maksim Shabanski <shabanskij.maksim@gmail.com>
2020-01-20 13:37:39 +0300

    feat: add normalize styles

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import WidgetContainer from 'containers/WidgetContainer';
 import DashboardContainer from 'containers/DashboardContainer';
 
+import './root.scss';
+
 const Root = ({ isWidgetActive }) => {
   return (
     <main>
